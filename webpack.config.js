59798b7a2585b3048b7fb83ead0697337e950fe3
MindtoEye <vvelsen@gmail.com>
2021-03-27 19:05:42 -0400

    Fixed a clash between dart and old configurations that used node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,15 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },      
       {
         test: /\.(png|svg|jpg|gif)$/,
